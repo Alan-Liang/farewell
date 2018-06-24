543058889aae2d08a319e91bc7d5aeeec89797a4
Alan-Liang <16379418+Alan-Liang@users.noreply.github.com>
2018-06-24 21:14:13 +0800

    allow compression

--- init.js
+++ init.js
@@ -3,6 +3,7 @@ var templRadio=$("template-radio").innerHTML;
 var templTF=$("template-text-field").innerHTML;
 var templSelect=$("template-select").innerHTML;
 var templSelectFrame=$("template-select-frame").innerHTML;
+var templCheckbox=$("template-checkbox").innerHTML;
 var inits=document.querySelectorAll("[data-init]");
 for(var i=0;i<inits.length;i++){
 	var div=inits[i];
@@ -24,6 +25,17 @@ for(var i=0;i<inits.length;i++){
 		var html="";
 		for(var j=1;j<args.length;j++){
 			html+=template(templTF,{
+				id:args[j].split("/")[0],
+				text:args[j].split("/")[1]
+			});
+		}
+		div.innerHTML+=html;
+		break;
+		
+		case "checkbox":
+		var html="";
+		for(var j=1;j<args.length;j++){
+			html+=template(templCheckbox,{
 				id:args[j].split("/")[0],
 				text:args[j].split("/")[1]
 			});
